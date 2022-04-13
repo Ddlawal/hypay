cadd5806658d51fb698d797f07f7fd9287b4802f
Muhammad Lawal Adamu <lawaldd@gmail.com>
2022-04-13 18:53:34 +0100

    product link temporary page

--- src/pages/store/products/[id].tsx
+++ src/pages/store/products/[id].tsx
@@ -1,18 +1,50 @@
 import React, { GetServerSideProps, NextPage } from 'next'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { LoadingPage } from '../../../components/Layout/LoadingPage'
+import { Logo } from '../../../components/Logo'
+import { useProducts } from '../../../hooks/useProducts'
+import { COLORS } from '../../../lib/constants/colors'
 
-const ProductView: NextPage<{ product: { id: string } }> = ({ product: { id } }) => {
-    console.log(id)
-    return <div>Product {id}</div>
+const ProductView: NextPage<{ id: string }> = ({ id }) => {
+    const { product, isLoading } = useProducts(id)
+    const router = useRouter()
+
+    if (isLoading) {
+        return <LoadingPage />
+    }
+
+    if (!isLoading && !product) {
+        router.push('/')
+        return null
+    }
+
+    return (
+        <>
+            <div className="mt-4 flex justify-center">
+                <Logo size={48} labelled={{ labelPosition: 'bottom' }} color={COLORS.PRIMARY} />
+            </div>
+            <div className="mt-16 flex items-start justify-between px-[20%]">
+                <div className="relative h-44 w-[50%] rounded-lg border border-gray-100 sm:h-60">
+                    <Image src={product?.image_url || ''} layout="fill" objectFit="cover" />
+                </div>
+                <div className="w-[40%]">
+                    <p>{product?.productName}</p>
+                    <p>{product?.quantity}</p>
+                    <p>{product?.amount}</p>
+                    <p>{product?.productDescription}</p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ProductView
 
-export const getServerSideProps: GetServerSideProps<{ product: { id: string } }> = async ({ query }) => {
+export const getServerSideProps: GetServerSideProps<{ id: string }> = async ({ query }) => {
     const id = query.id as string
 
     return {
-        props: {
-            product: { id },
-        },
+        props: { id },
     }
 }
