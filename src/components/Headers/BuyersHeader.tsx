dfcbf50722deb99b9ed22530de3e8cfec8694f98
Muhammad Lawal Adamu <lawaldd@gmail.com>
2022-05-28 06:26:50 +0100

    buyer header

--- src/components/Headers/BuyersHeader.tsx
+++ src/components/Headers/BuyersHeader.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
 import cx from 'classnames'
-import Image from 'next/image'
 import { useOnClickOutside } from '../../hooks/useOnClickOutSide'
 import { COLORS } from '../../lib/constants/colors'
 import { Input } from '../form'
-import { CommentIcon, MenuIcon, MoreOptionsVIcon, NotificationIcon, SearchIcon } from '../Icons'
+import { MenuIcon, SearchIcon } from '../Icons'
 import { BUYER_SIDE_NAV_WIDTH } from '../../lib/constants/elements'
-import { Dropdown } from '../Dropdown'
-import { dropdownMenuItems } from '../../lib/data'
-import { useRouter } from 'next/router'
 import { NextLink } from '../Links'
 import { Logo } from '../Logo'
 
@@ -19,32 +15,53 @@ export const BuyersHeader = () => {
     const width = BUYER_SIDE_NAV_WIDTH + 'px'
 
     return (
-        <div className="flex w-full items-center justify-between bg-white py-4 px-2 md:justify-end md:px-8">
+        <div className="flex items-center justify-between bg-white py-4 px-2 md:justify-end md:px-28">
             <div
                 ref={ref}
                 style={{ width }}
                 className={cx(
-                    !open && 'translate-x-full',
-                    'absolute left-[50%] top-0 block h-screen bg-hypay-light-blue transition duration-500 ease-in-out md:hidden'
+                    open && '-translate-x-full',
+                    'absolute right-[-200px] top-0 block h-screen bg-hypay-light-blue transition duration-500 ease-in-out md:hidden'
                 )}
             >
-                Side
+                <header className="flex w-full items-center justify-center pt-8 pb-16">
+                    <NextLink href="/dashboard/home">
+                        <Logo labelled={{ labelPosition: 'right' }} labelColor={'text-white'} color={COLORS.WHITE} />
+                    </NextLink>
+                </header>
+
+                <div className="text-bold w-full pl-4 text-xl text-white">
+                    <NextLink href="/store" className="block py-2">
+                        Home
+                    </NextLink>
+                    <NextLink href="/store/checkout" className="block py-2">
+                        Checkout
+                    </NextLink>
+                    <NextLink href="/store/support" className="block py-2">
+                        Atendimento
+                    </NextLink>
+                </div>
             </div>
-            <Logo size={24} />
-            <div className="flex items-center justify-end gap-x-2 md:gap-x-4">
+
+            <Logo size={24} labelled={{ labelPosition: 'right' }} />
+            <div className="text-bold ml-44 hidden w-full gap-x-20 pl-4 text-xl md:flex">
+                <NextLink href="/store">Home</NextLink>
+                <NextLink href="/store/checkout">Checkout</NextLink>
+                <NextLink href="/store/support">Atendimento</NextLink>
+            </div>
+            <div className="flex items-center justify-end gap-x-2 md:gap-x-0">
                 <Input
                     placeholder="Search"
-                    fullWidth
                     autoFocus
-                    className="bg-hypay-light-gray text-sm"
-                    parentClassName="mr-20 hidden md:block"
+                    className="w-60 bg-hypay-light-gray text-sm"
+                    parentClassName="hidden md:block"
                     icon={<SearchIcon color={COLORS.PLACEHOLDER} />}
                     type="search"
                 />
                 <button className=" block rounded-lg p-2 transition duration-200 ease-in-out hover:scale-105 hover:shadow-md md:hidden">
                     <SearchIcon size={26} />
                 </button>
-                <button className="p-2" onClick={() => setOpen(true)}>
+                <button className="p-2 md:hidden" onClick={() => setOpen(true)}>
                     <MenuIcon size={26} color={COLORS.ICON_GRAY} />
                 </button>
             </div>
