551d4017fdd979e69d758f14f58512ccfbee4765
Hassan Yakubu <55262580+yarcx@users.noreply.github.com>
2022-08-23 00:06:32 +0100

    Fixed the deployment error encountered on the auth services folder

--- src/store/services/auth.ts
+++ src/store/services/auth.ts
@@ -127,7 +127,7 @@ export const {
     useLogoutMutation,
     useCreateBusinessNameMutation,
     useLoginWithGoogleMutation,
-    useLazyLoginWithGoogleQuery,
+    // useLazyLoginWithGoogleQuery,
     useLazyVerifyEmailQuery,
     useLazyGenerateSecretQuery,
     useLazyEnableTwoFAQuery,
